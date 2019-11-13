2852e32d6f1006a29b0e8f8fd8e619cac187de54
dragoshomner <43915731+dragoshomner@users.noreply.github.com>
2019-11-13 22:30:27 +0200

    Add files via upload

---
+++ Lab homeworks/knock_detector.ino
@@ -0,0 +1,85 @@
+#include <JC_Button.h>
+
+const int SECOND = 1000;
+
+const byte 
+  PASIVE_BUZZER_PIN = A0,
+  ACTIVE_BUZZER_PIN = A1,
+  BUTTON_PIN = 2;
+
+const int 
+  pasiveBuzzerThreshold = 10,
+  activeBuzzerValue = 1000,
+  activeBuzzerDelay = 5;
+
+Button 
+  button(BUTTON_PIN);
+
+int pasiveBuzzerValue;
+bool buzzerWasKnocked = false;
+unsigned int previousTimeKnocked;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  
+  pinMode(PASIVE_BUZZER_PIN, INPUT);
+  pinMode(ACTIVE_BUZZER_PIN, OUTPUT);
+
+  button.begin();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  button.read();
+  
+  if(button.wasReleased()){
+    buzzerWasKnocked = false;
+    stopSong(ACTIVE_BUZZER_PIN);
+  }
+  
+  if(isKnocked(PASIVE_BUZZER_PIN)){
+    buzzerWasKnocked = true;
+    resetTimer();
+  }
+
+  if(timerIsFinished(previousTimeKnocked, activeBuzzerDelay * SECOND) && buzzerWasKnocked){
+    startSong(ACTIVE_BUZZER_PIN);
+  }
+}
+
+bool isKnocked(int pin)
+{
+  pasiveBuzzerValue = analogRead(pin);
+  if(pasiveBuzzerValue > pasiveBuzzerThreshold){
+    return true;
+  }
+  return false;
+}
+
+void resetTimer()
+{
+    previousTimeKnocked = millis();
+}
+
+bool timerIsFinished(int start, int duration)
+{
+  if(millis() - start >= duration){
+    return true;
+  }
+  return false;
+}
+
+void startSong(int pin)
+{
+  tone(pin, activeBuzzerValue);
+}
+
+void stopSong(int pin)
+{
+  noTone(pin);
+}
